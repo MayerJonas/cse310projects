06397580f8c087f2cfe0bcee6a97c501e296221e
MayerJonas <jns.myr1@gmail.com>
2026-09-10 21:20:43 -0700

    Create main.cpp
    bubbleSort algorithm for CSE310 Project 1

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream> // enables usage of std::cin and std::cout
+
+// bubbleSort function, is called by main, takes two arguments and returns void
+void bubbleSort(int arr[], int n) { // arguments are the array of elements and the number of elements n
+    for (int i = 0; i < n - 1; ++i) {
+        // We go through all elements but the last, because we always compare i with i+1
+        for (int j = 0; j < n - i - 1; ++j) {
+            // inner loop subtracts i because the largest elements add up already sorted in the end
+            if (arr[j] > arr[j + 1]) { // compare element at hand with element to the right and swap if order is unsorted
+                int tempVar = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tempVar;
+            }
+        }
+    }
+}
+
+// main function, reading input, calling bubbleSort, and printing results
+int main() {
+    int n; // number of elements
+    
+    if (!(std::cin >> n)) { // read n = number of elements, exit when no input is given
+        return 0;
+    }
+
+    int* arr = new int[n]; // array holding n elements
+
+    for (int i = 0; i < n; ++i) { // reading elements of the array from input
+        std::cin >> arr[i];
+    }
+
+    bubbleSort(arr, n); // invoking bubbleSort function
+
+    // printing the sorted array element by element, followed by a space
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    
+    std::cout << std::endl; // newline
+
+    delete[] arr; // free up memory
+
+    return 0; // exit program
+}
